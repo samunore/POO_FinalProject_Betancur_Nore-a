f2a9f44d70c61796e4787a1f0da2aca277c1773a
samunore <samuel.norena63313@ucaldas.edu.co>
2025-10-28 21:04:52 -0500

    Create VideoGame class with attributes, constructor, and stock control methods

--- src/domain/VideoGame.java
+++ src/domain/VideoGame.java
@@ -1,1 +1,50 @@
+package domain;
 
+import java.io.Serializable;
+
+public class VideoGame implements Serializable {
+    private String genre = "", title = "";
+    private Double price = 0.0;
+    private int amount = 0;
+
+    public VideoGame() {
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void increaseAmount(int amount) {
+        this.amount += amount;
+    }
+
+    public void decreaseAmount(int amount){
+        if (this.amount>=amount) {
+            this.amount-=amount;
+        }
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
