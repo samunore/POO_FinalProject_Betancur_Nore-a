d7883e02f03937c7972c65d1e5054759a42fe7c9
samunore <samuel.norena63313@ucaldas.edu.co>
2025-10-31 18:58:05 -0500

    Add Store class with confirmations

--- src/domain/Store.java
+++ src/domain/Store.java
@@ -1,18 +1,20 @@
 package domain;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Store implements Serializable {
-    private String name = "";
+    private String name = "La Cucha";
     private int saleRecord = 0;
     private Double incomeReport = 0.0;
+    private int idOrder = 0;
     private ArrayList<VideoGame> videoGames = new ArrayList<>();
+    private ArrayList<Order> orders = new ArrayList<>();
+    private ArrayList<Customer> customers = new ArrayList<>();
 
     public String getName() {
         return name;
     }
-    
+
     public int getSaleRecord() {
         return saleRecord;
     }
@@ -21,8 +23,60 @@ public class Store implements Serializable {
         return incomeReport;
     }
 
-    public void registerSale(double totalAmount) {
-    saleRecord++;
-    incomeReport += totalAmount;
+    public void addVideoGameStore(VideoGame game) {
+        for (VideoGame videoGame : videoGames) {
+            if (videoGame.getTitle().toLowerCase().equals(game.getTitle().toLowerCase())) {
+                if (game != null) {
+                    videoGames.add(game);
+                }
+            }
+        }
+    }
+
+    public ArrayList<VideoGame> availableVideoGames() {
+        return videoGames;
+    }
+
+    public ArrayList<VideoGame> searchTitle(String title) {
+        ArrayList<VideoGame> results = new ArrayList<>();
+        for (VideoGame game : videoGames) {
+            if (game.getTitle().toLowerCase().equals(title.toLowerCase())) {
+                results.add(game);
+            }
+        }
+        return results;
+    }
+
+    public ArrayList<VideoGame> searchGenre(String genre) {
+        ArrayList<VideoGame> results = new ArrayList<>();
+        for (VideoGame game : videoGames) {
+            if (game.getGenre().toLowerCase().equals(genre.toLowerCase())) {
+                results.add(game);
+            }
+        }
+        return results;
+    }
+
+    public int getIdOrder() {
+        idOrder++;
+        return idOrder;
+    }
+
+    public void addOrder(Order order) {
+        if (order != null) {
+            orders.add(order);
+            saleRecord++;
+            incomeReport += order.getTotalPrice();
+        }
+    }
+
+    public void addCustomer(Customer customer) {
+        for (Customer customer2 : customers) {
+            if (customer2.getCC() !=customer.getCC()) {
+                if (customer != null) {
+                    customers.add(customer);
+                }
+            }
+        }
     }
 }
