a41cb816e459874abdfab0a33365789de4415ed2
samunore <samuel.norena63313@ucaldas.edu.co>
2025-10-31 21:42:16 -0500

    Add StoreStorage class to handle saving and loading Store data

--- src/data/StoreStorage.java
+++ src/data/StoreStorage.java
@@ -1,1 +1,30 @@
+package data;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
+import domain.Store;
+
+public class StoreStorage {
+
+    public static void save(Store store, String fileName) {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            out.writeObject(store);
+            System.out.println("Datos de la tienda guardados correctamente.");
+        } catch (Exception e) {
+            System.err.println("Error al guardar los datos de la tienda: " + e.getMessage());
+        }
+    }
+
+    public static Store load(String filename) {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
+            return (Store) in.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error al cargar los datos de la tienda: " + e.getMessage());
+            return null;
+        }
+    }
+}
