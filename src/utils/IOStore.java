51cb302be27824af6cdccb8705bc27d415455789
samunore <samuel.norena63313@ucaldas.edu.co>
2025-11-05 13:59:51 -0500

    Implement IOStoreConsole class for console input and output handling

--- src/utils/IOStore.java
+++ src/utils/IOStore.java
@@ -1,1 +1,10 @@
+package utils;
+import java.util.ArrayList;
 
+public interface IOStore {
+    public abstract String inputText(String title);
+    public abstract int inputInt(String title);
+    public abstract double inputDouble(String title);
+    public abstract void showText(String message);
+    public abstract void showList(ArrayList<?> list);
+}
