a323036e96364377c520f887db0098f727b0e55e
samunore <samuel.norena63313@ucaldas.edu.co>
2025-11-06 17:23:25 -0500

    Add multiple showList

--- src/utils/IOStore.java
+++ src/utils/IOStore.java
@@ -1,10 +1,21 @@
 package utils;
 import java.util.ArrayList;
+import domain.Customer;
+import domain.Order;
+import domain.VideoGame;
 
 public interface IOStore {
     public abstract String inputText(String title);
+
     public abstract int inputInt(String title);
+
     public abstract double inputDouble(String title);
+
     public abstract void showText(String message);
-    public abstract void showList(ArrayList<?> list);
+
+    public abstract void showListVideoGames(ArrayList<VideoGame> videoGames);
+
+    public abstract void showListOrder(ArrayList<Order> orders);
+
+    public abstract void showListCustomer(ArrayList<Customer> customers);
 }
