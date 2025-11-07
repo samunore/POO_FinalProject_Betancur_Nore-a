ad994ba6a4184db178a3d12005943db3ca8421dc
samunore <samuel.norena63313@ucaldas.edu.co>
2025-11-07 15:09:02 -0500

    Add IOStoreDialog implementation for based input and output using JOptionPane

--- src/utils/IOStoreDialog.java
+++ src/utils/IOStoreDialog.java
@@ -27,24 +27,29 @@ public class IOStoreDialog implements IOStore {
         JOptionPane.showMessageDialog(null, message);
     }
 
+    StringBuilder sb = new StringBuilder();
+
     @Override
     public void showListVideoGames(ArrayList<VideoGame> videoGames) {
         for (VideoGame videoGame : videoGames) {
-
+            sb.append("Titulo: ").append(videoGame.getTitle()).append(", Genero: ").append(videoGame.getGenre()).append(", Precio: ").append(videoGame.getPrice()).append("\n");
         }
+        JOptionPane.showMessageDialog(null, sb.toString());
     }
 
     @Override
     public void showListOrder(ArrayList<Order> orders) {
         for (Order order : orders) {
-
+            sb.append("ID: ").append(order.getId()).append(", \n Precio total: ").append(order.getTotalPrice()).append("\n");
         }
+        JOptionPane.showMessageDialog(null, sb.toString());
     }
 
     @Override
     public void showListCustomer(ArrayList<Customer> customers) {
         for (Customer customer : customers) {
-
+            sb.append("Nombre: ").append(customer.getName()).append("C.C: ").append(customer.getName()).append("\n");
         }
+        JOptionPane.showMessageDialog(null, sb.toString());
     }
 }
