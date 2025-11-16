9771d67cd50947eb31490751c3036360021c344b
samunore <samuel.norena63313@ucaldas.edu.co>
2025-11-16 17:55:15 -0500

    Add multiple showList

--- src/utils/IOStore.java
+++ src/utils/IOStore.java
@@ -6,16 +6,11 @@ import domain.VideoGame;
 
 public interface IOStore {
     public abstract String inputText(String title);
-
-    public abstract int inputInt(String title);
-
-    public abstract double inputDouble(String title);
-
+    public abstract int inputInt(String numInt);
+    public abstract double inputDouble(String numDouble);
+    public abstract long inputLong(String numLong);
     public abstract void showText(String message);
-
     public abstract void showListVideoGames(ArrayList<VideoGame> videoGames);
-
     public abstract void showListOrder(ArrayList<Order> orders);
-
     public abstract void showListCustomer(ArrayList<Customer> customers);
 }
